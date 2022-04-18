25c146e76ed420a1c7ec76e5308e152c97ccf7e9
Hamdi Çatal <hamdicatal@outlook.com>
2022-04-18 23:05:58 +0300

    send class created.

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+class Send{
+    public static void Main(){
+        var factory = new ConnectionFactory() {
+            HostName = "localhost"
+        };
+
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel()){
+            channel.QueueDeclare(
+                queue: "hello",
+                durable: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "hello",
+                basicProperties: null,
+                body: body
+            );
+
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [Enter] to exit.");
+        Console.ReadLine();
+    }
+}
