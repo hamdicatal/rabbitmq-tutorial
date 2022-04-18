4994a1304db21a2d3d22c3e7141ad91b7e5e2a81
Hamdi Çatal <hamdicatal@outlook.com>
2022-04-18 23:11:15 +0300

    send class updated.

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,35 +1,32 @@
-﻿using System.Text;
+﻿using System;
 using RabbitMQ.Client;
+using System.Text;
 
-class Send{
-    public static void Main(){
-        var factory = new ConnectionFactory() {
-            HostName = "localhost"
-        };
-
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
         using(var connection = factory.CreateConnection())
-        using(var channel = connection.CreateModel()){
-            channel.QueueDeclare(
-                queue: "hello",
-                durable: false,
-                autoDelete: false,
-                arguments: null
-            );
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
 
             string message = "Hello World!";
             var body = Encoding.UTF8.GetBytes(message);
 
-            channel.BasicPublish(
-                exchange: "",
-                routingKey: "hello",
-                basicProperties: null,
-                body: body
-            );
-
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "hello",
+                                 basicProperties: null,
+                                 body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
 
-        Console.WriteLine(" Press [Enter] to exit.");
+        Console.WriteLine(" Press [enter] to exit.");
         Console.ReadLine();
     }
 }
